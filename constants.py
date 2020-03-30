bf556d398c972daca0310fca672d6170d790a44a
Vedant Puri <vedantpuri13@gmail.com>
2020-03-29 22:50:33 -0400

    Resolve #2, #3 + Formatting improvements

---
+++ constants.py
@@ -0,0 +1,6 @@
+import os
+
+URL = "https://tompkinscountyny.gov/health"
+CWD = os.getcwd() + "/"
+SUBSCRIBER_FILE = "recv.txt"
+MSG_FOOTER = "-Athish Rahul Rao\n+1 7189160665"
--- track.py
+++ track.py
@@ -3,141 +3,156 @@ import urllib3
 import hashlib
 import requests
 import smtplib, ssl
+from constants import *
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-URL = "https://tompkinscountyny.gov/health"
-USER = ""
-PWD = ""
-RECV = []
-when, totalTest, posCases, negCases, deaths = "", "", "", "", ""
-CWD = os.getcwd()
+# ----- GENERAL UTILS
+def retreive_subscribers():
+    """
+    Get a list of subscribers to whom the mail is to be sent to
 
-#####################################################################################################################
-# Method : Get a list of users to whom the mail is to be sent to
-# Enter email ID's seperated by newline character
-def retreiveUsers():
-    global RECV
-    with open(CWD+"/recv.txt", "r") as f:
+    :return:    Emails of current subscribers
+    """
+    subscribers = []
+    with open(CWD + SUBSCRIBER_FILE, "r") as f:
         for line in f.readlines():
-            RECV.append(line.strip())
+            subscribers.append(line.strip())
+    return subscribers
 
-#####################################################################################################################
-# Method : Create Body of the mail
-# Parameters - 
-# when          date of the announcement
-# totalTest     total test cases
-# posCases      positive cases
-# negCases      negative cases
-# deaths        deaths caused
-def getBody(when, totalTest, posCases, negCases, deaths):
-    return str("In Ithaca, as of " + when + ", Total Tested for COVID-19 is " + totalTest + " Positive Test Results are " + posCases + ". Negative cases is " + negCases + ". Death toll is " + deaths + ".\nTake care.\n\n-Athish Rahul Rao\n+1 7189160665")
-    
-#####################################################################################################################
-# Method : Retrieve gmail credentials from a file called creds.txt to be places in the same directory as this file
-# Username of line 1 and Pwd on line 2
-# Parameters - None
-def getCreds():
-    global USER
-    global PWD
-    creds = []
-    with open(CWD+"/creds.txt", "r") as f:
-        for line in f.readlines():
-            creds.append(line.strip())
-    USER = creds[0]
-    PWD = creds[1]
 
-#####################################################################################################################
-# Method : Get a unique hash for the numbs at hand
-# Parameters -
-# text          text to be hashed
-# Return - hash value of the text
-def getHash(text):
-    return str(hashlib.sha224(text.encode('utf-8')).hexdigest())
+def get_body(when, totalTest, posCases, negCases, deaths):
+    """
+    Create Body of the mail
 
-#####################################################################################################################
-# Method : Go through the contents of the page and identify the table
-# Parameters -
-# url       URL to the page
-def parseURL(url):
+    :param when:        date of the announcement
+    :param totalTest:   total test cases
+    :param posCases:    positive cases
+    :param negCases:    negative cases
+    :param deaths:      deaths caused
+
+    :return:            Formatted message string
+    """
+    return f"In Ithaca, as of {when} \nTotal Tested for COVID-19: {totalTest} Positive Test Results count: {posCases}\nNegative results count: {negCases}\nDeath toll: {deaths}\n\nTake care.\n\n{MSG_FOOTER}"
+
+
+def get_creds():
+    """
+    Load my email credentials from environment variables
+
+    :return:    tuple -> email, passWord
+    """
+    return os.environ.get("EMAIL_ID"), os.environ.get("EMAIL_PASS")
+
+
+def generate_hash(text):
+    """
+    Get a unique hash for the numbs at hand
+
+    :param text:    text to be hashed
+    :return:        SHA224 value of the text
+    """
+    return str(hashlib.sha224(text.encode("utf-8")).hexdigest())
+
+
+# ----- HTML PARSING UTILS
+def parse_url(url):
+    """
+    Go through the contents of the page and identify the table
+
+    :param url:     URL to the page
+    :return:        parse_table return
+    """
     response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, "lxml")
     temp = []
-    table = soup.find_all('table')[0]
-    parseTable(table)
+    table = soup.find_all("table")[0]
+    return parse_table(table)
 
-#####################################################################################################################
-# Method : Given a table, retrieve the numbers
-# Parameters -
-# table     HTML Table from parseURL method
-def parseTable(table):
-    global totalTest
-    global posCases
-    global negCases
-    global deaths
-    global when
+
+def parse_table(table):
+    """
+    Given a table, retrieve the numbers
+
+    :param table:       HTML Table from parse_url method
+    :return:            A hashmap (detail_type -> detail_value)
+    """
+    details = {}
     cols = 0
     rows = 0
     column_names = []
-    data = table.find_all('tr')
-    when = str(table.find_all("em")).split(".")[0][11:]
-    data = str(data).split("<td class=\"ctr\">")
+    data = table.find_all("tr")
+    details["when"] = str(table.find_all("em")).split(".")[0][11:]
+    data = str(data).split('<td class="ctr">')
     data = data[4].split()
-    totalTest = data[3].split("</td>")[0]
-    posCases = data[6].split("</td>")[0]
-    negCases = data[9].split("</td>")[0]
-    deaths = data[12].split("</td>")[0]
+    details["totalTest"] = data[3].split("</td>")[0]
+    details["posCases"] = data[6].split("</td>")[0]
+    details["negCases"] = data[9].split("</td>")[0]
+    details["deaths"] = data[12].split("</td>")[0]
+    return details
 
-#####################################################################################################################
-# Method : Send an email from the sender to recievers
-# Parameters -
-# Username      gmail username of sender
-# password      gmail password of sender
-# receivers     list of receivers
-# body          body of the mail being sent
-def sendMail(Username, password, receivers, body):
-    
+
+# ----- MAILER
+def dispatch_mails(username, password, receivers, body):
+    """
+    Send an email from the sender to recievers
+
+    :param username:      gmail username of sender
+    :param password:      gmail password of sender
+    :param receivers:     list of receivers
+    :param body:          body of the mail being sent
+    """
     receivers = ",".join(receivers)
     message = MIMEMultipart()
-    message['From'] = Username
-    message['To'] = receivers
-    message['Subject'] = 'Ithaca Corona Update '+ when
-    message.attach(MIMEText(body, 'plain'))
-    session = smtplib.SMTP('smtp.gmail.com', 587) 
-    session.starttls() 
-    session.login(Username, password) 
+    message["From"] = username
+    message["To"] = receivers
+    message["Subject"] = "Ithaca Corona Update " + when
+    message.attach(MIMEText(body, "plain"))
+    session = smtplib.SMTP("smtp.gmail.com", 587)
+    session.starttls()
+    # TODO: NEED ERROR CHECKING HERE (LOGIN FAILED)
+    session.login(Username, password)
     text = message.as_string()
+    # TODO: NEED ERROR CHECKING HERE (EMAIL SEND FAILED)
     session.sendmail(Username, receivers, text)
     session.quit()
 
-#####################################################################################################################
-# Method : Called on initialisation of the script
+
+# ----- MAIN EXECUTION
 if __name__ == "__main__":
     oldHash = ""
     message = ""
-    with open(CWD + "/hash.txt", "r") as f:
+    with open(CWD + "hash.txt", "r") as f:
         for line in f.readlines():
             oldHash = line.strip()
-    
-    retreiveUsers()
-    
-    parseURL(URL)
 
-    getCreds()
+    subscribers = retreive_subscribers()
+    parsed_data = parse_url(URL)
+    username, password = get_creds()
 
-    hash = getHash(when + totalTest + posCases + negCases + deaths)
-    
-    body = getBody(when, totalTest, posCases, negCases, deaths)
-
-    if (hash == oldHash):
-        message = 'No Updates!'
+    hash = generate_hash(
+        parsed_data["when"]
+        + parsed_data["totalTest"]
+        + parsed_data["posCases"]
+        + parsed_data["negCases"]
+        + parsed_data["deaths"]
+    )
+    if hash == oldHash:
+        message = "No Updates!"
     else:
-        sendMail(USER, PWD, RECV, body)
-        message = 'Mail Sent!'
-        with open(CWD + "/hash.txt", "w") as f:
+        body = get_body(
+            parsed_data["when"],
+            parsed_data["totalTest"],
+            parsed_data["posCases"],
+            parsed_data["negCases"],
+            parsed_data["deaths"],
+        )
+        dispatch_mails(username, password, subscribers, body)
+        message = "Mail Sent!"
+        with open(CWD + "hash.txt", "w") as f:
             f.write(hash)
-    
-    with open(CWD + "/file.txt", "a") as f:
-            f.write(message)
+
+    with open(CWD + "file.txt", "a") as f:
+        f.write(message)
